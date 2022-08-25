ac2d86892a6b70261e7e824e6f1d94d3f407f59a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-08-26 08:36:57 +0900

    Update Alice-and-Bob-s-Silly-Game.cpp

--- Alice-and-Bob-s-Silly-Game.cpp
+++ Alice-and-Bob-s-Silly-Game.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sillyGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER n as parameter.
+ */
+
+// from https://www.hackerrank.com/challenges/alice-and-bobs-silly-game/forum by RAZAK_123
+string sillyGame(int n)
+{
+  return "Bob";
+}
