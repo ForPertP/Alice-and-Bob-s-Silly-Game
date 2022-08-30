2601657eaf7778d8babf26a427c908a520ee7f3e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-08-31 08:26:10 +0900

    Update Alice-and-Bob-s-Silly-Game.cpp

--- Alice-and-Bob-s-Silly-Game.cpp
+++ Alice-and-Bob-s-Silly-Game.cpp
@@ -17,3 +17,51 @@ string sillyGame(int n)
 {
   return "Bob";
 }
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string g_temp;
+    getline(cin, g_temp);
+
+    int g = stoi(ltrim(rtrim(g_temp)));
+
+    for (int g_itr = 0; g_itr < g; g_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string result = sillyGame(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
