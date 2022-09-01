8a386aebb3d5eca0222211515d82d8ad4f3b07f4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-09-01 10:34:39 +0900

    Update Alice-and-Bob-s-Silly-Game.cpp

--- Alice-and-Bob-s-Silly-Game.cpp
+++ Alice-and-Bob-s-Silly-Game.cpp
@@ -15,9 +15,43 @@ string rtrim(const string &);
 // from https://www.hackerrank.com/challenges/alice-and-bobs-silly-game/forum by RAZAK_123
 string sillyGame(int n)
 {
-  return "Bob";
+    if (n == 1)
+    {
+        return "Bob";
+    }
+    
+    int dp[n+1];
+    dp[0] = 0 ;
+    dp[1] = 0 ;
+    dp[2] = 1 ;
+    
+    for (int i = 3; i <= n; ++i)
+    {
+        if (i & 1)
+        {
+            dp[i] = 1;
+        }
+    }
+    
+    for (int i = 2; i <= sqrt(n); ++i)
+    {
+        for (int j = 2; i*j <= n; ++j)
+        {
+            dp[i*j] = 0;
+        }
+    }
+    
+    int cnt = 0;
+    for (int i = 0; i <= n; ++i)
+    {
+        if (dp[i])
+        {
+            cnt++;
+        }
+    }
+    
+    return (cnt&1) ? "Alice" : "Bob";
 }
-
 
 int main()
 {
